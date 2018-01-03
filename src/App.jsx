e270ea72d310b40e053540e3d32f05d88ce15827
Edward Melkoyev <edmelkoyev@gmail.com>
2018-01-03 23:09:06 +0300

    Props and prop-types validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
-    return ();
+function App(props) {
+  return (
+    <main>
+      <header>
+        <h1>{props.title}</h1>
+      </header>
+
+      <section className="todo-list">
+        <div className="todo completed">
+          <button className="checkbox icon">
+            <i className="material-icons">check_box</i>
+          </button>
+          <span className="todo-title">To Learn JavaScript</span>
+          <button className="delete icon">
+            <i className="material-icons">delete</i>
+          </button>
+        </div>
+        <div className="todo">
+          <button className="checkbox icon">
+            <i className="material-icons">check_box_outline_blank</i>
+          </button>
+          <span className="todo-title">To Learn React</span>
+          <button className="delete icon">
+            <i className="material-icons">delete</i>
+          </button>
+        </div>
+      </section>
+    </main>
+  );
 }
+
+App.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+App.defaultProps = {
+  title: 'Reaact todo default'
+};
 
 export default App;
