744bc42817c304d78001e1fdc5492b5dc316ef1a
Edward Melkoyev <edmelkoyev@gmail.com>
2018-01-03 21:40:16 +0300

    Component App has been created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <h1>React Todo</h1>
+          <p className="subtitle">This is my first React JSX Appication</p>
+      </div>
+    );
+  }
+}
+
+export default App;
